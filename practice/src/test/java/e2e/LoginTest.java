2fe08085d6cabc08db6b1f3a08ef0168975bb29d
Shekhar <reach2shekhar@gmail.com>
2025-04-20 10:11:39 +0530

    added screesnhot logic

--- practice/src/test/java/e2e/LoginTest.java
+++ practice/src/test/java/e2e/LoginTest.java
@@ -1,8 +1,14 @@
 package e2e;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,53 +18,59 @@ import org.testng.annotations.Test;
 // This class contains test cases to verify the login functionality of the Naukri website.
 public class LoginTest {
 
-    @Test
-    // It checks if the user can log in with valid credentials and ensures
-    // redirection to the homepage.
-    // This test verifies the successful login behavior on the Naukri website.
-    public void loginTest() throws InterruptedException {
+        @Test
+        // It checks if the user can log in with valid credentials and ensures
+        // redirection to the homepage.
+        // This test verifies the successful login behavior on the Naukri website.
+        public void loginTest() throws InterruptedException, IOException {
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        options.addArguments("--disable-blink-features=AutomationControlled");
-        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
-        WebDriver driver = new ChromeDriver(options);
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless=new");
+                options.addArguments("--disable-blink-features=AutomationControlled");
+                options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+                WebDriver driver = new ChromeDriver(options);
 
-        // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
+                // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.manage().window().maximize();
 
-        driver.manage().deleteAllCookies();
+                driver.manage().deleteAllCookies();
 
-        driver.get("https://www.naukri.com/");
-        System.out.println("opend successfully");
+                driver.get("https://www.naukri.com/");
+                System.out.println("opend successfully");
 
-        driver.findElement(By.xpath("//a[@title='Jobseeker Login']")).click();
+                driver.findElement(By.xpath("//a[@title='Jobseeker Login']")).click();
 
-        System.out.println("clicked successfully");
+                System.out.println("clicked successfully");
 
-        driver.findElement(By.xpath("//input[@placeholder='Enter your active Email ID / Username']"))
-                .sendKeys(System.getProperty("login.username"));
+                driver.findElement(By.xpath("//input[@placeholder='Enter your active Email ID / Username']"))
+                                .sendKeys(System.getProperty("login.username"));
 
-        driver.findElement(By.xpath("//input[@placeholder='Enter your password']"))
-                .sendKeys(System.getProperty("login.password"));
+                driver.findElement(By.xpath("//input[@placeholder='Enter your password']"))
+                                .sendKeys(System.getProperty("login.password"));
 
-        driver.findElement(By.xpath("//button[@class='btn-primary loginButton']"))
-                .click();
+                driver.findElement(By.xpath("//button[@class='btn-primary loginButton']"))
+                                .click();
 
-        // Assert
-Thread.sleep(5000);
-        // wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".reco-title")));
-        String actual = driver.getCurrentUrl();
+                // Capture Screenshot
+                TakesScreenshot screenshot = (TakesScreenshot) driver;
+                File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+                File destFile = new File("target/dashboard_screenshot.png");
+                Files.copy(srcFile.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
-        System.out.println("current url is " + actual);
+                // Assert
+                Thread.sleep(5000);
+                // wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".reco-title")));
+                String actual = driver.getCurrentUrl();
 
-        Assert.assertTrue(actual.equals("https://www.naukri.com/mnjuser/homepage"));
+                System.out.println("current url is " + actual);
 
-        System.out.println("successfully validated");
+                Assert.assertTrue(actual.equals("https://www.naukri.com/mnjuser/homepage"));
 
-        driver.quit();
+                System.out.println("successfully validated");
 
-    }
+                driver.quit();
+
+        }
 
 }
