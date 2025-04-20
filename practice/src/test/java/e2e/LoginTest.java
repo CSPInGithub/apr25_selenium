03f2ee57edf3dc0bd5309ec58eac76806bd543d2
Shekhar <reach2shekhar@gmail.com>
2025-04-20 11:08:46 +0530

    added screeshot

--- practice/src/test/java/e2e/LoginTest.java
+++ practice/src/test/java/e2e/LoginTest.java
@@ -28,16 +28,16 @@ public class LoginTest {
 
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--headless=new");
-                options.addArguments("--window-size=1920,1080");
-                options.addArguments("--disable-gpu");
-                options.addArguments("--no-sandbox");
-                options.addArguments("--disable-dev-shm-usage");
+                // options.addArguments("--window-size=1920,1080");
+                // options.addArguments("--disable-gpu");
+                // options.addArguments("--no-sandbox");
+                // options.addArguments("--disable-dev-shm-usage");
                 options.addArguments("--disable-blink-features=AutomationControlled");
                 options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
                 WebDriver driver = new ChromeDriver(options);
 
                 // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
                 driver.manage().window().maximize();
 
                 driver.manage().deleteAllCookies();
@@ -49,8 +49,6 @@ public class LoginTest {
 
                 ((JavascriptExecutor) driver).executeScript("arguments[0].click();", jsLogin);
 
-                
-
                 System.out.println("js login clicked successfully");
 
                 driver.findElement(By.xpath("//input[@placeholder='Enter your active Email ID / Username']"))
@@ -60,12 +58,10 @@ public class LoginTest {
                                 .sendKeys(System.getProperty("login.password"));
 
                 WebElement loginBtn = driver.findElement(By.xpath("//button[@class='btn-primary loginButton']"));
-
-
-                ((JavascriptExecutor) driver).executeScript("arguments[0].click();", loginBtn);
+                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", loginBtn);
+                loginBtn.click();
 
                 System.out.println("login clicked successfully after entering credentials..");
-
                 // Capture Screenshot
                 TakesScreenshot screenshot = (TakesScreenshot) driver;
                 File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
@@ -73,11 +69,15 @@ public class LoginTest {
                 Files.copy(srcFile.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
                 // Assert
-                Thread.sleep(9000);
+                Thread.sleep(6000);
                 // wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".reco-title")));
                 String actual = driver.getCurrentUrl();
 
                 System.out.println("current url is " + actual);
+                TakesScreenshot screenshot1 = (TakesScreenshot) driver;
+                File srcFile1 = screenshot1.getScreenshotAs(OutputType.FILE);
+                File destFile1 = new File("target/dashboard_screenshot1.png");
+                Files.copy(srcFile1.toPath(), destFile1.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
                 Assert.assertTrue(actual.equals("https://www.naukri.com/mnjuser/homepage"));
 
